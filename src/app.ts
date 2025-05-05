103977b6b3d907b10af80d27b80cd91c0396aba9
haazemsaid <haazemsaidd@gmail.com>
2025-05-05 14:25:21 +0200

    refactor: use environment

--- src/app.ts
+++ src/app.ts
@@ -59,6 +59,7 @@ app.use((req: Request, res: Response) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
